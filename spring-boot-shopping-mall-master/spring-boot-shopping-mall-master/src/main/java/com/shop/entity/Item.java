eef055ce16f4f4e2162b0e910d30dad14d002f19
saeyeonLee <108654869+saeyeonLee@users.noreply.github.com>
2023-01-20 11:55:53 +0900

    Add files via upload

---
+++ spring-boot-shopping-mall-master/spring-boot-shopping-mall-master/src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,60 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.dto.ItemFormDto;
+import com.shop.exception.OutOfStockException;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
+@Getter @Setter
+@ToString
+public class Item extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "item_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String itemName;
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stock;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;
+
+    public void updateItem(ItemFormDto itemFormDto) {
+        this.itemName = itemFormDto.getItemName();
+        this.price = itemFormDto.getPrice();
+        this.stock = itemFormDto.getStock();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
+
+    public void removeStock(int stock) {
+
+        int restStock = this.stock - stock;
+        if (restStock < 0) {
+            throw new OutOfStockException("상품의 재고가 부족합니다. (현재 재고 수량: " + this.stock + ")");
+        }
+        this.stock = restStock;
+    }
+
+    public void addStock(int stock) {
+        this.stock += stock;
+    }
+
+}
